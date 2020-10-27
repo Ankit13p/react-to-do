5caaf81532c3d9b9e4a95d947543335f72e6e3bd
Ankitkumar Prajapati <42886562+Ankit13p@users.noreply.github.com>
2020-10-27 13:37:56 +1300

    react to do application

---
+++ precache-manifest.936886077c981de706859ba0b9f42179.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8faf5a22ac53e2b3fb356f430e679b45",
+    "url": "/react-to-do/index.html"
+  },
+  {
+    "revision": "c25b66f59b9b395c3b2e",
+    "url": "/react-to-do/static/css/main.a2059918.chunk.css"
+  },
+  {
+    "revision": "6adc341bc1185725c044",
+    "url": "/react-to-do/static/js/2.d610cbc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-to-do/static/js/2.d610cbc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c25b66f59b9b395c3b2e",
+    "url": "/react-to-do/static/js/main.c3717076.chunk.js"
+  },
+  {
+    "revision": "34bc805e57a78bde92bb",
+    "url": "/react-to-do/static/js/runtime-main.b92a3415.js"
+  }
+]);
